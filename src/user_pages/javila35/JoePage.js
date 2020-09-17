553e4d0cd96c8e3d44803f9677eba18445be8d24
javila35 <josepharthuravila@gmail.com>
2020-09-17 13:29:12 -0700

    Showing joe's recently played music.

--- src/user_pages/javila35/JoePage.js
+++ src/user_pages/javila35/JoePage.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import MusicDisplay from './components/MusicDisplayer/MusicDisplayer';
 
 const JoePage = () => {
+    const mainStyle = {
+        backgroundColor: 'grey'
+    }
+    
 
     return (
-        <>
+        <div style={mainStyle}>
 
         <h1>Hey, my name is Joe Avila!</h1> 
         
         <p>I'm a developer out of Seattle, WA. I have a cat named Ozma. I enjoy games of all sorts: sports, video and board.</p>
         <p>I'm looking forward to working with you!</p>
 
-        </>
-        
+        <MusicDisplay />
+        </div>
     );
 };
 
